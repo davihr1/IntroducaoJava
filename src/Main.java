fe83be8e77fc248d90e0cb9866ca48a39c0682df
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-02-26 18:35:30 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import static java.lang.System.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int valor = 8;
+
+        if(valor == 9) {
+            out.println("valor = 8");
+        }
+        else {
+            out.println("É um valor diferente");
+        }
+    }
+}
